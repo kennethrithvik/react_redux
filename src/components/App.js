563f84745463bc4c3c80a5434ca82ffaa4d837d0
Kenneth Rithvik <kennethrithvik@yahoo.in>
2017-05-23 16:07:12 +0530

    test

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,19 @@
 import React, {PropTypes} from 'react';
 import Header from './common/Header';
 
-class App extends React.Component{
-	render(){
-		return(
-			<div className="container-fluid">
-			   <h1>React and Redux in ES6 on Pluralsight</h1>
-			   <Header/>
-			  {this.props.children}
-			</div>
-		);
-	}
+class App extends React.Component {
+		render() {
+				return (
+						<div className="container-fluid">
+								<h1>React and Redux in ES6 on Pluralsight</h1>
+								<Header/> {this.props.children}
+						</div>
+				);
+		}
 }
 
-App.propTypes={
-	children: PropTypes.object.isRequired
+App.propTypes = {
+		children: PropTypes.object.isRequired
 };
 
 export default App;
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,6 @@
 import {combineReducers} from 'redux';
 import courseReducer from './courseReducer';
 
-const rootReducer= combineReducers({
-	courses:courseReducer
-});
+const rootReducer = combineReducers({courses: courseReducer});
 
 export default rootReducer;
